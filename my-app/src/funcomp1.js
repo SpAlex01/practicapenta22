5fddd36f00d843eeee977896e63b28e94bda87b6
Spulber Alex <alexspulber291@gmail.com>
2022-07-31 20:12:12 +0300

    g

--- my-app/src/funcomp1.js
+++ my-app/src/funcomp1.js
@@ -1,16 +1,31 @@
 import React, { useState } from "react"
 
 
-const Functionalcomp=()=>{
-    const [color,setColor]=useState("black")
-    const handleClick=()=>{
 
-        setColor("white")
-    }
-    
+const Functionalcomp=()=>{
+    const [color,setColor]=useState("white")
+    const handleClick1=()=>{
+
+    return setColor("yellow")
+
+  }
+  const handleClick2=()=>{
+
+    return setColor("pink")
+
+  }
+
+
+   
     return (
-        <><div><button onClick={handleClick}>Click  here</button></div><div> <p>{color}</p></div></>
+        <div style={{backgroundColor:color}}>
+        <button onClick={handleClick1}>Click here</button>
+        <button onClick={handleClick2}>Click here</button>
+        {<p>{color}</p>}
+        </div>
     )
+
+    
 }
 
 export default Functionalcomp
